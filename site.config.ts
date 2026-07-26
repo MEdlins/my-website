0748eb7652c2be8922b8c81dfaf0bd2c0b5b3758
MEdlins <mariglynn@gmail.com>
2026-07-26 11:21:01 -0400

    fix runtime to nodejs

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '51c3f5f0fa3f83f98a3e017838c27905',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Mariglynn Edlins, PhD',
+  domain: 'Mariglynn.com',
+  author: 'Mariglynn Edlins',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Mariglynn Edlins, PhD',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
